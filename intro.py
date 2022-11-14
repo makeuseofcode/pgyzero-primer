21b64a1f3811166924ceda096761f39a63c580ed
Elijah <89408204+diywriter@users.noreply.github.com>
2022-11-13 21:32:45 -0500

    updated file with annotations

--- intro.py
+++ intro.py
@@ -1,27 +1,25 @@
-# from turtle import window_width
+#this python file requires pgzero - installation instructions: https://pygame-zero.readthedocs.io/en/latest/installation.html
 
+alien = Actor('alien') #the actor points to the png file in /images/alien.png
 
-alien = Actor('alien')
-# alien.pos = 120, 56
 alien.topright = 0, 75
 
 
-WIDTH = alien.width + 300
-HEIGHT = alien.height + 150
+WIDTH = alien.width + 300 #sets the window width in relation to the sprite
+HEIGHT = alien.height + 150 #sets the window height  in relation to the sprite
 
 def draw():
     screen.clear()
     alien.draw()
 
-def update():
-    alien.left += 5
-    # alien.right += 5
-    if alien.right > WIDTH:
+def update(): #update every frame
+    alien.left += 5 #moves sprint along the screen
+    if alien.right > WIDTH: #checks to see when sprite reaches end of window
        alien.right = 0
     
 
-def on_mouse_down(pos):
-    if alien.collidepoint(pos):
+def on_mouse_down(pos):  #this function is checking to see when the mouse cursor position matches the sprite position to simulate an impact
+    if alien.collidepoint(pos): 
         print("Eek!")
     else:
         print("You missed me!")
